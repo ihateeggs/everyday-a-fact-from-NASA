4da25debf94f736a6b38f366e6773a2ec6063cd9
ihateeggs <daikia17@gmail.com>
2022-05-11 11:37:02 +0700

    First commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import requests
+import sys
+
+original_stdout = sys.stdout  
+response = requests.get("https://api.nasa.gov/planetary/apod?api_key=ETNDm36Voef3ibJEg1RupywYcoIF86DfQx3vH58e")
+
+param = response.json() 
+ 
+# with open('demo.txt', 'w') as f:
+#     sys.stdout = f 
+#     print (param['date'] + "\n")
+#     print (param['explanation'])
+#     print("\n")
+#     print("Here is link of" + param['title'] + ": " + param['url'])
+#     # Reset the standard output
+#     sys.stdout = original_stdout
+
+# print("The information has already in the demo.txt file")
+
+print (f"[{param['date']}]" + "\n")
+print (param['explanation'])
+print("\n")
+print("Here is link of " + param['title'] + ": " + param['url'] + "\n")
+print("Have a nice day!!!")
